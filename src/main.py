9c7258439ee0ab2226aa21d5c01ac8b71bc305b4
Nikitchenko Nikita <nikitos13.n@gmail.com>
2026-01-29 02:05:49 +0400

    add caching get response with ttl

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 from starlette import status
+
+from src.core.config import settings
 from src.exceptions import NonexistentUrlException, SlugAlreadyExistsException
 from src.database.db import engine, new_session
 from src.database.models import Base
@@ -11,18 +13,25 @@ from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from contextlib import asynccontextmanager
 from src.service import generate_short_url, get_url_by_slug
+from src.redis_client import redis_client, RedisClient
 
+
+async def get_redis_client():
+    return redis_client
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with new_session() as session:
+        yield session
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
+    await redis_client.connect()
     yield
-    ...
+    await redis_client.disconnect()
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
-    async with new_session() as session:
-        yield session
 
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=True, allow_methods=["*"])
@@ -43,9 +52,14 @@ async def short_url(
 
 @app.get("/{slug}")
 async def redirect_to_url(slug: str,
+                          redis: Annotated[RedisClient, Depends(get_redis_client)],
                           session: Annotated[AsyncSession, Depends(get_session)]):
     try:
+        cached_url = await redis.get(slug)
+        if cached_url:
+            return RedirectResponse(url=cached_url, status_code=status.HTTP_302_FOUND)
         url = await get_url_by_slug(slug, session=session)
+        await redis.set(slug, url, settings.cache.ttl_slug)
     except NonexistentUrlException:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found.")
     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
@@ -57,10 +71,10 @@ if __name__ == "__main__":
 
 
 """
+!ограничитель запросов
 валидация url(бывают ссылки в приложениях, например tg://)
-создание человекочитаемых кастомных ссылок
+! создание человекочитаемых кастомных ссылок
 аналитика переходов
-обработка коллизий слагов
 время жизни ссылки
 redis?
 логи
@@ -68,6 +82,8 @@ redis?
 select for update
 генерация не рандомно, а на основе ссылки?
 несуществующие не определяет
+
+обработка коллизий слагов(решено в 5 ретраев)
 
 база данных для тестов
 инверсия зависимостей, обёртка над сессией бд, свой класс
