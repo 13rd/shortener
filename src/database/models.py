f5bbae40ff1f864422c3642a2ac04b12d40e5083
Nikitchenko Nikita <nikitos13.n@gmail.com>
2026-02-03 05:12:17 +0400

    update model for lifetime url

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String
+from datetime import datetime, timedelta, UTC
+from sqlalchemy import Column, String, DateTime, Index
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 class Base(DeclarativeBase):
@@ -9,4 +10,12 @@ class ShortUrl(Base):
     __tablename__ = 'short_urls'
 
     slug: Mapped[str] = mapped_column(primary_key=True)
-    long_url: Mapped[str]
+    long_url: Mapped[str] = mapped_column(String(2048))
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now(UTC)
+    )
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    __table_args__ = (
+        Index('idx_expires_at', 'expires_at'),
+        )
